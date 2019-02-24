7410129873ef6e5cc664eea355e8153359107a21
Pratik Sahindrakar <pratik.sahindrakar@figmd.com>
2019-02-24 19:47:58 +0530

    Jest config ignore less files pattern updated

--- packages/react-scripts/scripts/utils/createJestConfig.js
+++ packages/react-scripts/scripts/utils/createJestConfig.js
@@ -55,11 +55,11 @@ module.exports = (resolve, rootDir, isEjecting) => {
     },
     transformIgnorePatterns: [
       '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$',
-      '^.+\\.module\\.(css|sass|scss)$',
+      '^.+\\.module\\.(css|less|sass|scss)$',
     ],
     moduleNameMapper: {
       '^react-native$': 'react-native-web',
-      '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+      '^.+\\.module\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     },
     moduleFileExtensions: [...paths.moduleFileExtensions, 'node'].filter(
       ext => !ext.includes('mjs')
